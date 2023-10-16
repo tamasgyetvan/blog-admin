d7078a458b59c07cc3c63c5ba273f535d011de21
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-16 13:59:14 +0200

    Navbar renders only if client has a token

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,20 +7,22 @@ export function Header() {
   return (
     <header>
       <h1>blog.</h1>
-      <nav>
-        <NavLink to="/create_post">Create post</NavLink>
-        <NavLink to="/edit_post">Edit post</NavLink>
-        <NavLink to="/remove_post">Remove Post</NavLink>
-        <button
-          onClick={() => {
-            localStorage.removeItem("token");
-            window.location.reload();
-          }}
-          type="submit"
-        >
-          Log out
-        </button>
-      </nav>
+      {localStorage.getItem("token") ? (
+        <nav>
+          <NavLink to="/create_post">Create post</NavLink>
+          <NavLink to="/edit_post">Edit post</NavLink>
+          <NavLink to="/remove_post">Remove Post</NavLink>
+          <button
+            onClick={() => {
+              localStorage.removeItem("token");
+              window.location.reload();
+            }}
+            type="submit"
+          >
+            Log out
+          </button>
+        </nav>
+      ) : null}
     </header>
   );
 }
