22d5ab5f1f44d9dbdba1efa24d8eebd0b474a62c
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-16 13:56:40 +0200

    Add page reload after user logs out to make conditional routes work

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,9 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Navigate, redirect, useNavigate } from "react-router-dom";
 import "./header.scss";
 
 export function Header() {
+  const navigate = useNavigate();
+
   return (
     <header>
       <h1>blog.</h1>
@@ -9,7 +11,15 @@ export function Header() {
         <NavLink to="/create_post">Create post</NavLink>
         <NavLink to="/edit_post">Edit post</NavLink>
         <NavLink to="/remove_post">Remove Post</NavLink>
-        <button type="submit">Log out</button>
+        <button
+          onClick={() => {
+            localStorage.removeItem("token");
+            window.location.reload();
+          }}
+          type="submit"
+        >
+          Log out
+        </button>
       </nav>
     </header>
   );
