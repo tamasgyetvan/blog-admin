0002d4b023accc5f5742a0c992de9c0299ff2f53
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-18 11:00:55 +0200

    Create fetch request to retrieve posts from database

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 export function AdminPage() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [blogData, setBlogData] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/get_posts", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => setBlogData(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  });
+  if (error) return <p>Network error... please reload!</p>;
+  if (loading) return <p>Loading...</p>;
   return (
     <>
-      <h1>Adminpage</h1>
+      <h2>Blog data will be rendered here...</h2>
     </>
   );
 }
