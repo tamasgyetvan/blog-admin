9167dfe655029666c28312160863261715ef90aa
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-20 14:39:18 +0200

    Rework style of PostCard component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -14,14 +14,18 @@ export function PostCard({ data, handleDeleteClick }: PostCardProps) {
         <h3 className="postTitle">{data.title}</h3>
         <p className="postAuthor">{data.author.username}</p>
         <p className="creationDate">{data.timestamp}</p>
-      </div>
-      <div className="actionContainer">
-        <button>
-          <img className="editIcon" src="/edit-3-svgrepo-com.svg" alt="" />
-        </button>
-        <button onClick={handleDeleteClick}>
-          <img className="deleteIcon" src="/delete-2-svgrepo-com.svg" alt="" />
-        </button>
+        <div className="actionContainer">
+          <button>
+            <img className="editIcon" src="/edit-3-svgrepo-com.svg" alt="" />
+          </button>
+          <button onClick={handleDeleteClick}>
+            <img
+              className="deleteIcon"
+              src="/delete-2-svgrepo-com.svg"
+              alt=""
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
