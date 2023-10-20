abb81ac35b32a8ed8642d4abd739df573b52193b
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-20 14:48:52 +0200

    Add wrapper to AuthenticationPage for centering

--- src/pages/AuthenticationPage.tsx
+++ src/pages/AuthenticationPage.tsx
@@ -1,5 +1,11 @@
 import Authenticator from "../components/authentication/Authenticator";
+import "./AuthenticationPage.scss";
+import "../App.scss";
 
 export function AuthenticationPage() {
-  return <Authenticator></Authenticator>;
+  return (
+    <div className="authContainer">
+      <Authenticator></Authenticator>
+    </div>
+  );
 }
