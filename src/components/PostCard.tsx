778c2f783d9911b105ac8cec744624ec2773b6ec
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-20 09:26:28 +0200

    Rework fetch request url

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,9 +5,30 @@ type PostCardProps = {
 };
 
 export function PostCard({ data }: PostCardProps) {
+  const handleDeleteClick = async (e: React.MouseEvent<HTMLElement>) => {
+    const token: string | null = localStorage.getItem("token");
+    const target = e.target as Element | null;
+    const id: string | undefined = target?.closest(".postCard")?.id;
+    fetch(`http://localhost:3000/api/delete_post/${id}`, {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.errorMessage) {
+          alert(data.errorMessage);
+        } else {
+          alert(data);
+        }
+      });
+  };
   return (
-    <div key={data._id} className="postCard">
-      <img src="public/image-picture-svgrepo-com.svg" alt="" />
+    <div id={data._id} className="postCard">
+      <img src="/image-picture-svgrepo-com.svg" alt="" />
       <div className="infoContainer">
         <h3 className="postTitle">{data.title}</h3>
         <p className="postAuthor">{data.author.username}</p>
@@ -15,18 +36,10 @@ export function PostCard({ data }: PostCardProps) {
       </div>
       <div className="actionContainer">
         <button>
-          <img
-            className="editIcon"
-            src="public/edit-3-svgrepo-com.svg"
-            alt=""
-          />
+          <img className="editIcon" src="/edit-3-svgrepo-com.svg" alt="" />
         </button>
-        <button>
-          <img
-            className="deleteIcon"
-            src="public/delete-2-svgrepo-com.svg"
-            alt=""
-          />
+        <button onClick={handleDeleteClick}>
+          <img className="deleteIcon" src="/delete-2-svgrepo-com.svg" alt="" />
         </button>
       </div>
     </div>
