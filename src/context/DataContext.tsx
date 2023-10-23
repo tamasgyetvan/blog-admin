187700c2abb317f3199ffa33bfed060a1e9c41b0
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-23 21:18:21 +0200

    Create addBlogItem and removeBlogItem functions

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -4,17 +4,36 @@ type DataContextProviderProps = {
   children: ReactNode;
 };
 
-export const DataContext = createContext({
-  data: [],
-  errors: null,
-  loading: true,
-});
+type BlogPost = {
+  _id: string;
+  author: object;
+  text: string;
+  timestamp: string;
+  title: string;
+};
+
+type DataContext = {
+  data: Array<BlogPost>;
+  loading: boolean;
+  error: string | null;
+  addBlogItem: (item: BlogPost) => void;
+  removeBlogItem: (id: string) => void;
+};
+
+export const DataContext = createContext({} as DataContext);
 
 export function DataContextProvider({ children }: DataContextProviderProps) {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Array<BlogPost>>([]);
   const [loading, setLoading] = useState(true);
-  const [errors, setErrors] = useState(null);
+  const [error, setErrors] = useState(null);
   const token = localStorage.getItem("token");
+
+  const addBlogItem = (item: BlogPost) => {
+    setData([...data, item]);
+  };
+  const removeBlogItem = (id: string) => {
+    setData(data.filter((data) => data._id !== id));
+  };
 
   useEffect(() => {
     fetch("http://localhost:3000/api/queryposts", {
@@ -39,7 +58,9 @@ export function DataContextProvider({ children }: DataContextProviderProps) {
   }, []);
 
   return (
-    <DataContext.Provider value={{ data, errors, loading }}>
+    <DataContext.Provider
+      value={{ data, error, loading, addBlogItem, removeBlogItem }}
+    >
       {children}
     </DataContext.Provider>
   );
