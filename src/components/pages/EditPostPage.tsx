11831972893e362db253054116e9d6122f9c264a
tamasgyetvan98 <117265815+tamasgyetvan@users.noreply.github.com>
2023-10-27 11:39:08 +0200

    Fix undefined error on page refresh

--- src/components/pages/EditPostPage.tsx
+++ src/components/pages/EditPostPage.tsx
@@ -1,6 +1,86 @@
+import { useContext, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
+import { DataContext } from "../../context/DataContext";
+import { useForm, FieldValues } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
+import "./EditPostPage.scss";
 
 export function EditPostPage() {
   const params = useParams();
-  return <h1>{params.id}</h1>;
+  const { error, loading, data } = useContext(DataContext);
+  const [editorContent, setEditorContent] = useState({
+    content: "",
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleEditorChange = (content: any) => {
+    setEditorContent({ content });
+  };
+
+  const onSubmit = async (data: FieldValues) => {};
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  const postToEdit = data.filter((data) => data._id === params.id)[0];
+  return (
+    <>
+      <form className="editPostForm" onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Title
+          <input
+            {...register("title", {
+              required: "Title is required.",
+              maxLength: {
+                value: 30,
+                message: "Title cannot be longer than 25 characters",
+              },
+            })}
+            type="text"
+            value={postToEdit.title}
+          ></input>
+          {errors.title && <span>{`${errors.title.message}`}</span>}
+        </label>
+        <Editor
+          value={postToEdit.text}
+          onEditorChange={handleEditorChange}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "preview",
+              "anchor",
+              "searchreplace",
+              "visualblocks",
+              "code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "code",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+        />
+        <button type="submit">Edit Post</button>
+      </form>
+    </>
+  );
 }
